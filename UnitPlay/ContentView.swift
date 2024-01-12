493bec4883d891e1820dc6c697de686e9dd92975
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-12 01:24:21 -0500

    Unit converter app

--- UnitPlay/ContentView.swift
+++ UnitPlay/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput = Measurement(value: 0.0, unit: UnitLength.meters)
+    @State private var sourceUnit = UnitLength.meters
+    @State private var outputUnit = UnitLength.centimeters
+    
+    let units: [UnitLength] = [.centimeters, .meters, .kilometers]
+    
+    var conversion: Measurement<UnitLength> {
+        return userInput.converted(to: outputUnit)
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Convert from") {
+                    Picker("Unit", selection: $sourceUnit) {
+                        ForEach (units, id: \.self) { unit in
+                            Text(unit.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Convert to") {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach (units, id: \.self) { unit in
+                            Text(unit.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Input") {
+                    TextField("User Input", value: $userInput.value, formatter: NumberFormatter())
+                }
+                Section("Converted Result") {
+                    Text("\(conversion.value, format: .number)")
+                }
+            }
+            .navigationTitle("Unit Converter")
         }
-        .padding()
     }
 }
 
