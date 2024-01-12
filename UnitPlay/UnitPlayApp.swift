3f4c809f1d8b40e454fdfbeb6eb4bcb0f69a573d
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-11 23:42:16 -0500

    Initial Commit

---
+++ UnitPlay/UnitPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitPlayApp.swift
+//  UnitPlay
+//
+//  Created by Mit Sheth on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
